a1f540fd9491c312fea54fc4a8f26798526f979e
Raul Manendez <muk214782@gmail.com>
2023-04-02 12:32:23 +0530

    docker compose up -d

--- docker-front-end/src/app/department/department.component.ts
+++ docker-front-end/src/app/department/department.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Department } from '../model/department.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-department',
@@ -18,7 +19,7 @@ export class DepartmentComponent implements OnInit {
   }
 
   getDepartments(){
-    this.http.get<Department[]>('http://localhost:8000/docker/department').subscribe((response) => {
+    this.http.get<Department[]>(`${environment.BASE_URL}department`).subscribe((response) => {
       this.departments=response;
     })
   }
